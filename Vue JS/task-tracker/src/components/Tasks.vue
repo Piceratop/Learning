de7ded30ff27d96599ba45c8a5023ffa81d47194
MawatariTanaka <buinhatminh2005@gmail.com>
2022-10-22 21:18:54 +0700

    Update all

---
+++ Vue JS/task-tracker/src/components/Tasks.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :key="task.id" v-for="task in tasks">
+    <Task @delete-task="$emit('delete-task', task.id)" @toggle-reminder="$emit('toggle-reminder', task.id)"
+      :task="task" />
+  </div>
+</template>
+
+<script>
+import Task from './Task.vue';
+export default {
+  name: "Tasks",
+  props: {
+    tasks: Array,
+  },
+  components: { Task },
+  emits: ['delete-task', 'toggle-reminder']
+}
+</script>
