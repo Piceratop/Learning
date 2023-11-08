3cfeede3501c9c93e3fe0abde6968a8aa047bc4f
Piceratop Ultimate <buinhatminh2k52005@gmail.com>
2023-11-08 22:55:59 +0700

    add

--- Edcoder Learning Python Advanced Exercises/02.py
+++
@@ -1,5 +0,0 @@
-def calculate(num):
-    return sum([i for i in range(num) if i % 5 == 0 or i % 7 == 0])
-
-
-print(calculate(100))
--- Edcoder Learning Python Advanced Exercises/06.py
+++
@@ -1,5 +0,0 @@
-def calculate():
-    return max([i * j for i in range(10, 100) for j in range(10, 100) if str(i * j) == str(i * j)[::-1]])
-
-
-print(calculate())
