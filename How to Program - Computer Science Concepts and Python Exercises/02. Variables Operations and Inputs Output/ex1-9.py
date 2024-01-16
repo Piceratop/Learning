dc41296b3d59023953c7fedab35496964d4c6abb
Piceratop Ultimate <buinhatminh2k52005@gmail.com>
2024-01-17 00:17:18 +0700

    Add ex1-9

---
+++ How to Program - Computer Science Concepts and Python Exercises/02. Variables Operations and Inputs Output/ex1-9.py
@@ -0,0 +1,53 @@
+# Exercises 1 - 9
+
+# 25
+a = 10
+b = 15
+a += b
+print(a)
+
+# 15
+a = 10
+b = 15
+a = b
+b = 1
+print(a)
+
+# 115
+a = 10
+b = 15
+a = a*a+b
+print(a)
+
+# 250
+a = 10
+b = 15
+a *= a+b
+print(a)
+
+# 10.0
+a = 10
+b = float(a)
+print(b)
+
+# 10
+a = "10"
+b = int(a)
+print(b)
+
+# Welcome Home
+a = "Welcome"
+b = "Home"
+print(a,b)
+
+# WelcomeHome
+a = "Welcome"
+b = "Home"
+print(a+b)
+
+# 1015
+a = "10"
+b = "15"
+c = a+b
+d = int(c)
+print(d)
---
+++ How to Program - Computer Science Concepts and Python Exercises/02. Variables Operations and Inputs Output/lecture.py
@@ -0,0 +1,6 @@
+radius = float(input("Enter the radius of the circle: "))
+Pi = 3.14159
+area = Pi * radius * radius
+print("The area of the circle is", area)
+# OR
+print("The area of the circle is " + str(area))
