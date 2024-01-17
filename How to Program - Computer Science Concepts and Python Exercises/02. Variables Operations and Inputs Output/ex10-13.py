f07fab1198b973db615273fe761026aa18432098
Piceratop Ultimate <buinhatminh2k52005@gmail.com>
2024-01-17 10:52:47 +0700

    Finish chapter 2

---
+++ How to Program - Computer Science Concepts and Python Exercises/02. Variables Operations and Inputs Output/ex10-13.py
@@ -0,0 +1,18 @@
+# Exercise 10 - 13
+
+# 10
+bread_price = 2.00
+
+# 11
+cheese_price = 3.00
+cost = 2 * bread_price + 3 * cheese_price
+print(cost)
+
+# 12
+age = int(input("Enter your age: "))
+print(age)
+
+# 13
+name = input("Enter a knight's name: ")
+characteristic = input("Enter a knight's characteristic: ")
+print("Sir", name, "the", characteristic)
