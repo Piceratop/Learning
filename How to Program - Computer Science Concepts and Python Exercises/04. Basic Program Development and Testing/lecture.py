c09860cfdc2c5cfb8cbfeafbc54466da8ec5f9e0
Piceratop Ultimate <buinhatminh2k52005@gmail.com>
2024-01-20 14:05:56 +0700

    Add random config to python

---
+++ How to Program - Computer Science Concepts and Python Exercises/04. Basic Program Development and Testing/lecture.py
@@ -0,0 +1,29 @@
+# Get information from user
+print("I'll help you determine how long you will need to save.")
+name = input("What's your name? ")
+item = input("What is it you are saving up for? ")
+balance = float(input("OK, " + name + ". Please enter the cost of the " + item + ": "))
+if balance < 0:
+    print("Looks like you already have saved enough!")
+    balance = 0
+    payment = 1
+else:
+    payment = float(input("Enter how much you will save each period: "))
+    if payment <= 0:
+        payment = float(
+            input("Savings must be positive. Please enter a positive value: ")
+        )
+        if payment <= 0:
+            print(
+                name
+                + ", you still don't enter a positive number! I am going to just assume you save 1 per period."
+            )
+            payment = 1
+# Calculate number of payments that will be needed
+num_remaining_payments = balance / payment
+# Present information to user
+print(
+    name + ", you must make",
+    num_remaining_payments,
+    "more payments, and then you'll have your " + item + "!",
+)
