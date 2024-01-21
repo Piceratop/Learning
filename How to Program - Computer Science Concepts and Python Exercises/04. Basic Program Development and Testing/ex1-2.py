db2403f4fd1afbe76c260d2d45cee9f01a2b7ced
Piceratop Ultimate <buinhatminh2k52005@gmail.com>
2024-01-21 10:15:25 +0700

    Finish part 04

--- How to Program - Computer Science Concepts and Python Exercises/04. Basic Program Development and Testing/ex1-2.py
+++ How to Program - Computer Science Concepts and Python Exercises/04. Basic Program Development and Testing/ex1-2.py
@@ -2,13 +2,20 @@
 print("I'll help you determine how long you will need to save.")
 name = input("What's your name? ")
 item = input("What is it you are saving up for? ")
-balance = float(input("OK, " + name + ". Please enter the cost of the " + item + ": "))
+total_cost = float(
+    input("OK, " + name + ". Please enter the cost of the " + item + ": ")
+)
+saved = float(input("How much have you saved? "))
+balance = total_cost - saved
+frequency = ""
 if balance < 0:
     print("Looks like you already have saved enough!")
     balance = 0
     payment = 1
 else:
     payment = float(input("Enter how much you will save each period: "))
+    frequency = input("Enter how often you will save: ")
+
     if payment <= 0:
         payment = float(
             input("Savings must be positive. Please enter a positive value: ")
@@ -27,5 +34,5 @@ if num_remaining_payments < balance / payment:
 print(
     name + ", you must make",
     num_remaining_payments,
-    "more payments, and then you'll have your " + item + "!",
+    "more payments every " + frequency + ", and then you'll have your " + item + "!",
 )
