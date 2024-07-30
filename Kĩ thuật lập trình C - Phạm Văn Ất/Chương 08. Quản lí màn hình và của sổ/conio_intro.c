c75104169528cf682ea0ca0998d6b9f1c40d4b8c
Piceratop Finfina <buinhatminh2k52005@gmail.com>
2024-07-30 22:02:33 +0700

    MISSING upload

---
+++ Kĩ thuật lập trình C - Phạm Văn Ất/Chương 08. Quản lí màn hình và của sổ/conio_intro.c
@@ -0,0 +1,15 @@
+#include <conio.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+   system("cls");
+   _getch();  // Wait for another key press before continuing
+
+   printf("Using conio.h");
+
+   // Wait for input
+   _getch();
+
+   return 0;
+}
