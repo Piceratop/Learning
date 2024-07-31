82bc9a832f0fb3c0ed7b83c03b26833f01849a1e
Piceratop Finfina <buinhatminh2k52005@gmail.com>
2024-07-31 11:42:52 +0700

    Hoàn thành bài ví dụ 1, hai bản, phù hợp với Windows 11 với gcc của mingw

---
+++ Kĩ thuật lập trình C - Phạm Văn Ất/Chương 01. Các khái niệm cơ bản/010401B1.C
@@ -0,0 +1,15 @@
+/* Chương trình in ra 2 dòng chữ. Bản 1 (010401B1.C) */
+#include <stdio.h> /* Sự dụng thư viện vào ra chuẩn */
+#include <conio.h>
+#include <stdlib.h>
+
+int main() /* Hàm chính */
+{
+   system("cls"); /* Xóa màn hinh */
+   printf("\nTURBO C HAN HANH");
+   /* Xuống dòng và in: LAM QUEN VOI BAN */
+   printf("\nLAM QUEN VOI BAN");
+   getch(); /* Tạm dừng máy để xem kết quả */
+
+   return 0;
+}
---
+++ Kĩ thuật lập trình C - Phạm Văn Ất/Chương 01. Các khái niệm cơ bản/010401B2.C
@@ -0,0 +1,16 @@
+/* Chuong trinh in ra 2 dong chu. Ban 2 (010401B2.C) */
+#include <stdio.h> /* Su dung thu vien vao ra chuan */
+#include <conio.h>
+#include <stdlib.h>
+
+int main() /* Ham chinh */
+{
+   system("cls");
+   /* Xuong dong, in: TURBO C HAN HANHk
+   lai xuong dong roi in: LAM QUEN VOI BAN */
+   printf("\nTURBO C HAN HANH \
+   \nLAM QUEN VOI BAN");
+   getch(); /* Tam dung may de xem ket qua */
+
+   return 0;
+}
